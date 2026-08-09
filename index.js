fb669fc45905b284b4b2e7669535a5ea6a601bad
tebomabusetsa-ops <tebomabusetsa@gmail.com>
2026-08-09 15:35:24 +0200

    Added axios to HTML

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 let form = document.querySelector("#career-form");
 let input = document.querySelector("#career-input");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let userGoal = input.value;
+
+    console.log(userGoal);
+});
